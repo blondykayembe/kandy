3711db43b9d071f0639808876120a176e73a6271
blondykayembe@gmail.com <blondykayembe@gmail.com>
2023-09-22 12:58:13 +0200

    adding package "future" in namespace

--- R/kandy-package.R
+++ R/kandy-package.R
@@ -2,6 +2,9 @@
 #'
 #' @importFrom mvtnorm dmvnorm
 #' @importFrom future.apply future_sapply
+#' @importFrom future plan
+#' @importFrom future multisession
+#' @importFrom future availableCores
 "_PACKAGE"
 
 ## usethis namespace: start
